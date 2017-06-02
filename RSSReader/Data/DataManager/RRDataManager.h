dc6badd663c1073e78c457a98dae7e11dc81cb59
Kuznetsov Aleksey <aleksey.d.kuznetsov@gmail.com>
2017-06-02 17:06:24 +0300

    add DataManager and preset channels

---
+++ RSSReader/Data/DataManager/RRDataManager.h
@@ -0,0 +1,16 @@
+//
+//  RRDataManager.h
+//  RSSReader
+//
+//  Created by Kuznetsov Aleksey on 02.06.17.
+//  Copyright © 2017 Kuznetsov Aleksey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RRDataManager : NSObject
+
++ (instancetype)sharedInstance;
+
+
+@end
---
+++ RSSReader/Data/DataManager/RRDataManager.m
@@ -0,0 +1,49 @@
+//
+//  RRDataManager.m
+//  RSSReader
+//
+//  Created by Kuznetsov Aleksey on 02.06.17.
+//  Copyright © 2017 Kuznetsov Aleksey. All rights reserved.
+//
+
+#import "RRDataManager.h"
+#import <MagicalRecord/MagicalRecord.h>
+#import "RRChannel+CoreDataClass.h"
+
+@implementation RRDataManager
+
++ (instancetype)sharedInstance
+{
+    static RRDataManager *_sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedManager = [[self alloc] init];
+    });
+    return _sharedManager;
+}
+
+- (instancetype)init
+{
+    if ((self = [super init])){
+        [self createPresetChannelsIfNeeded];
+    }
+    return self;
+}
+
+- (void)createPresetChannelsIfNeeded
+{
+    NSArray *allCannels = [RRChannel MR_findAll];
+    if (!allCannels.count) {
+        NSArray *channels = [NSArray arrayWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"PresetChannels" ofType:@"plist"]];
+        for (int i = 0; i<channels.count; i++) {
+            RRChannel *channel = [RRChannel MR_createEntity];
+            channel.index = i;
+            channel.link = channels[i];
+        }
+    }
+}
+
+#pragma mark - --Data
+
+
+@end
