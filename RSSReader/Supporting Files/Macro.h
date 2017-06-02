354485f2adf24d62a075869a528be048bdcbf922
Kuznetsov Aleksey <aleksey.d.kuznetsov@gmail.com>
2017-06-02 15:55:51 +0300

    add Macro.h and PrefixHeader.pch

---
+++ RSSReader/Supporting Files/Macro.h
@@ -0,0 +1,15 @@
+//
+//  Macro.h
+//  RSSReader
+//
+//  Created by Kuznetsov Aleksey on 02.06.17.
+//  Copyright © 2017 Kuznetsov Aleksey. All rights reserved.
+//
+
+#ifndef Macro_h
+#define Macro_h
+
+#pragma mark - --Paths
+#define CORE_DATA_STORE_NAME @"RSSReader.coreData"
+
+#endif /* Macro_h */
